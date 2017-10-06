45d21ab23346f99c0bdb44fe4bfec22ea345d792
Yoan Thirion <yoan.thirion@gmail.com>
2017-10-06 14:48:41 +0200

    Add 1 integration test

---
+++ SandTigerShark.Functions.Tests/HttpRequest.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+
+namespace SandTigerShark.Functions.Tests
+{
+    public static class HttpRequest
+    {
+        public static HttpRequestMessage Create<T>(T objectToPost)
+        {
+            var json = JsonConvert.SerializeObject(objectToPost);
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+            return request;
+        }
+    }
+}
---
+++ SandTigerShark.Functions.Tests/TicTacToe/Integration_Tests.cs
@@ -0,0 +1,27 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SandTigerShark.Functions.Tests.TicTacToe
+{
+    //http://www.ben-morris.com/writing-unit-tests-for-azure-functions-using-c/
+    [TestClass]
+    public class TicTacToe_function
+    {
+        [TestMethod]
+        public async Task returns_a_200_OK_for_a_valid_play()
+        {
+            var command = PlayCommand.New();
+            var request = HttpRequest.Create(command);
+
+            var result = await Functions.TicTacToe.TicTacToe.Run(request, A.Fake<TraceWriter>());
+            var response = (HttpResponseMessage)result;
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
